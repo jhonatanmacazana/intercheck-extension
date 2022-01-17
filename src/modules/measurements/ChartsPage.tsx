d1ded6e11b488bc113f2b93ada30990886d9f85f
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2022-01-17 15:57:54 -0500

    feat: latency and download speed selector

--- src/modules/measurements/ChartsPage.tsx
+++ src/modules/measurements/ChartsPage.tsx
@@ -7,15 +7,17 @@ import { CartesianGrid, LineChart, Line, Tooltip, XAxis, YAxis } from "recharts"
 import { DataStorage } from "#root/modules/extension/dataStorage";
 import { TimeRecord } from "#root/modules/extension/types";
 import LoadingPage from "#root/components/LoadingPage";
-import { PING_STORAGE_KEY, UPDOWN_DOWNLOAD_STORAGE_KEY } from "#root/lib/constants";
+// import { PING_STORAGE_KEY, UPDOWN_DOWNLOAD_STORAGE_KEY } from "#root/lib/constants";
+
+type Option = "ping" | "download";
 
 const ChartsPage = () => {
   const navigate = useNavigate();
 
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedOption, setSelectedOption] = useState<Option>("ping");
   const [pingData, setPingData] = useState<TimeRecord[]>([]);
-  const [, setDownloadData] = useState<TimeRecord[]>([]);
-  // const [downloadData, setDownloadData] = useState<PayloadRecord[]>([]);
+  const [downloadData, setDownloadData] = useState<TimeRecord[]>([]);
 
   const setupData = useCallback(async () => {
     setIsLoading(true);
@@ -34,32 +36,32 @@ const ChartsPage = () => {
     setupData();
   }, []);
 
-  // Subscribe to live changes
-  useEffect(() => {
-    (async () => {
-      const listener: (
-        changes: { [key: string]: chrome.storage.StorageChange },
-        areaName: "sync" | "local" | "managed"
-      ) => void = (changes, areaName) => {
-        for (let [key, { newValue }] of Object.entries(changes)) {
-          if (key === PING_STORAGE_KEY && areaName === "local") {
-            setPingData(newValue);
-            // const difference = newValue.filter((x: any) => !oldValue.includes(x)) || [];
-            // setPingData(old => [...old, ...difference]);
-          }
+  // // Subscribe to live changes
+  // useEffect(() => {
+  //   (async () => {
+  //     const listener: (
+  //       changes: { [key: string]: chrome.storage.StorageChange },
+  //       areaName: "sync" | "local" | "managed"
+  //     ) => void = (changes, areaName) => {
+  //       for (let [key, { newValue }] of Object.entries(changes)) {
+  //         if (key === PING_STORAGE_KEY && areaName === "local") {
+  //           setPingData(newValue);
+  //           // const difference = newValue.filter((x: any) => !oldValue.includes(x)) || [];
+  //           // setPingData(old => [...old, ...difference]);
+  //         }
 
-          if (key === UPDOWN_DOWNLOAD_STORAGE_KEY && areaName === "local") {
-            setDownloadData(newValue);
-            // const difference = newValue.filter((x: any) => !oldValue.includes(x)) || [];
-            // setDownloadData(old => [...old, ...difference]);
-          }
-        }
-      };
+  //         if (key === UPDOWN_DOWNLOAD_STORAGE_KEY && areaName === "local") {
+  //           setDownloadData(newValue);
+  //           // const difference = newValue.filter((x: any) => !oldValue.includes(x)) || [];
+  //           // setDownloadData(old => [...old, ...difference]);
+  //         }
+  //       }
+  //     };
 
-      chrome.storage.onChanged.addListener(listener);
-      return () => chrome.storage.onChanged.removeListener(listener);
-    })();
-  }, []);
+  //     chrome.storage.onChanged.addListener(listener);
+  //     return () => chrome.storage.onChanged.removeListener(listener);
+  //   })();
+  // }, []);
 
   if (isLoading) {
     return <LoadingPage subtitle="Medición avanzada" />;
@@ -73,7 +75,11 @@ const ChartsPage = () => {
       </Heading>
 
       <VStack alignItems="center" justifyContent="space-around" w="100%">
-        <LineChart width={500} height={300} data={pingData}>
+        <LineChart
+          width={500}
+          height={300}
+          data={selectedOption === "ping" ? pingData : downloadData}
+        >
           <XAxis
             dataKey="t"
             domain={["auto", "auto"]}
@@ -85,7 +91,11 @@ const ChartsPage = () => {
           />
           <YAxis
             dataKey="v"
-            label={{ value: "Latencia (ms)", angle: -90, position: "insideLeft" }}
+            label={{
+              angle: -90,
+              value: selectedOption === "ping" ? "Latencia (ms)" : "Velocidad de Descarga (Mbps)",
+              position: "insideLeft",
+            }}
             name="Valor"
           />
           {/* <YAxis yAxisId={1} dataKey="v" name="Valor" label="Mbps" /> */}
@@ -94,13 +104,13 @@ const ChartsPage = () => {
           <Line
             activeDot={true}
             animationDuration={500}
-            data={pingData}
+            data={selectedOption === "ping" ? pingData : downloadData}
             dataKey="v"
             dot={false}
-            name="Latencia"
+            name={selectedOption === "ping" ? "Latencia" : "Velocidad de Descarga"}
             stroke="#8884d8"
             type="linear"
-            unit="ms"
+            unit={selectedOption === "ping" ? "ms" : "Mbps"}
           />
           {/* <Line
             yAxisId={1}
@@ -111,6 +121,23 @@ const ChartsPage = () => {
             stroke="#88f4d8"
           /> */}
         </LineChart>
+
+        <ButtonGroup isAttached paddingBottom="1rem">
+          <Button
+            colorScheme="blue"
+            isDisabled={selectedOption === "ping"}
+            onClick={() => setSelectedOption("ping")}
+          >
+            Latencia
+          </Button>
+          <Button
+            colorScheme="blue"
+            isDisabled={selectedOption === "download"}
+            onClick={() => setSelectedOption("download")}
+          >
+            Velocidad de descarga
+          </Button>
+        </ButtonGroup>
 
         <ButtonGroup paddingBottom="2rem">
           <Button onClick={() => navigate("/")}>Ir atrás</Button>
